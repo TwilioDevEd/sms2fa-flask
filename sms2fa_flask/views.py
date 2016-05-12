c9da9e9c732d8405cc75c913b6636434e264243b
Victor Shyba <victor.shyba@gmail.com>
2016-05-12 18:46:18 -0300

    Refactor views for readability

--- sms2fa_flask/views.py
+++ sms2fa_flask/views.py
@@ -33,27 +33,28 @@ def sign_up():
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.get(form.email.data)
-        if user:
-            if user.password_valid(form.password.data.encode('utf8')):
-                session['user_email'] = user.email
-                return redirect(url_for('confirmation'))
+        if user and user.password_valid(form.password.data.encode('utf8')):
+            session['user_email'] = user.email
+            return redirect(url_for('confirmation'))
         flash('Wrong user/password.', 'error')
+
     return render_template('sign_in.html', form=form)
 
 
 @app.route('/confirmation', methods=['GET', 'POST'])
 def confirmation():
-    user_id = session.get('user_email', None)
-    if not user_id:
+    user = User.query.get(session.get('user_email', ''))
+    if not user:
         flask.abort(401)
-    user = User.query.get(user_id)
+
     if request.method == 'POST':
-        given_code = request.form['verification_code']
-        if given_code == session['confirmation_code']:
+        if request.form['verification_code'] == session['confirmation_code']:
             login_user(user)
             return redirect(url_for('secret_page'))
+
     code = send_confirmation_code(user.international_phone_number)
     session['confirmation_code'] = code
     return render_template('confirmation.html', user=user)
