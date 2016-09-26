c20c876b973700ab6f3f8b934ce8dca3e482765d
Agustin Camino <agustin.camino@gmail.com>
2016-09-26 16:26:09 -0500

    Bump Twilio library to version 6.x

--- tests/confirmation_sender_tests.py
+++ tests/confirmation_sender_tests.py
@@ -9,11 +9,11 @@ class ConfirmationSenderTest(BaseTest):
 
     def test_sender_creates_a_message(self):
         confirmation_sender.generate_code = MagicMock(return_value='random_code')
-        with patch('twilio.rest.resources.messages.Messages.create') as create_mock:
+        with patch('twilio.rest.api.v2010.account.message.MessageList.create') as create_mock:
             confirmation_sender.send_confirmation_code('+15551234321')
             create_mock.assert_called_once_with(
-                    body='random_code',
+                    u'+15551234321',
                     from_=self.app.config['TWILIO_NUMBER'],
-                    to=u'+15551234321'
+                    body='random_code'
             )
             self.assertEquals('random_code', session.get('verification_code'))
